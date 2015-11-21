05b9b22ab7ab25920361228e38d9685b9d486d10
tasuku <tasuku@gmail.com>
2015-11-21 23:07:59 +0900

    edited comments

--- m3and5.hs
+++ m3and5.hs
@@ -1,4 +1,4 @@
---Sum of all the multiples of 3 or 5 below specified n
 --Project Euler Problem 1
+--Sum of all the multiples of 3 or 5 below specified n
 m3and5 :: Integer -> Integer
 m3and5 n = sum [x | x <- [1..n], x `mod` 3 == 0 || x `mod` 5 == 0]
