d31342295f75c8cfa1707339a5329a9aa29fa93e
tasuku <tasuku@gmail.com>
2015-11-21 23:09:01 +0900

    Project Euler Problem 2

---
+++ evenFibo.hs
@@ -0,0 +1,14 @@
+--Project Euler Problem 2
+--Even Fibonacci numbers
+--Calculates the sum of the even-valued terms of the Fibonacci sequence whose values do not exceed n.
+evenFibo :: Integer -> Integer
+evenFibo n = sum [x | x <- takeWhile (<n) (map (\ y -> fibo y) [1..]), even x]
+
+fibo :: Integer -> Integer
+fibo 0 = 1 
+fibo 1 = 1
+fibo n = fibo (n-1) + fibo (n-2)
+
+
+
+
